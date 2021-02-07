1deb1d56382c8edbd5a0d6b6910d394ff864a4d7
romanolivi <romano_livi@outlook.com>
2021-02-07 00:24:22 -0500

    Added Controllers, Views, and and Models

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,80 @@
 class TweetsController < ApplicationController
-
-
+  
+    get "/tweets" do 
+      if session[:user_id]
+        @tweets = Tweet.all 
+        erb :"/tweets/tweets"
+      else 
+        redirect "/login"
+      end
+    end
+    
+    get "/tweets/new" do 
+      if logged_in? 
+        erb :"/tweets/new"
+      else 
+        redirect to "/login"
+      end
+    end
+    
+    post "/tweets" do 
+      # binding.pry
+      if !params[:content].empty?
+        @new_tweet = Tweet.create(content: params[:content])
+        @new_tweet.user = current_user 
+        @new_tweet.save 
+        redirect to "/tweets/#{@new_tweet.id}"
+      else 
+        redirect to "/tweets/new"
+      end
+    end
+    
+    get "/tweets/:id" do 
+      if logged_in? 
+        @tweet = Tweet.find(params[:id])
+        erb :"/tweets/show_tweet"
+      else 
+        redirect to "/login"
+      end
+    end
+  
+    get '/tweets/:id/edit' do
+        if logged_in?
+          @tweet = Tweet.find_by_id(params[:id])
+          if @tweet && @tweet.user == current_user
+            erb :'tweets/edit_tweet'
+          else
+            redirect to '/tweets'
+          end
+        else
+          redirect to '/login'
+        end
+    end
+    
+    
+    
+    patch "/tweets/:id" do 
+      @tweet = Tweet.find(params[:id])
+      if params[:content].empty?
+        redirect to "/tweets/#{@tweet.id}/edit"
+      elsif !params[:content].empty?
+        @tweet.update(content: params[:content])
+        @tweet.save 
+        redirect to "/tweets/#{@tweet.id}"
+      elsif !logged_in?
+        redirect "/login"
+      end
+    end
+      
+    delete "/tweets/:id" do 
+      @tweet = Tweet.find(params[:id])
+      if @tweet.user == current_user
+        @tweet.delete
+        redirect to "/tweets"
+      else 
+        redirect "/tweets/#{@tweet.id}"
+      end
+    end
+  
+      
 end
